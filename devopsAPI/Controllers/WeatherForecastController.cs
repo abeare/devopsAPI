e439a31d36c3d5112ac4a599fa0a73405226106a
anur abubeker osman <abeare21@hotmail.com>
2022-08-10 16:11:53 +0200

    first

--- devopsAPI/Controllers/WeatherForecastController.cs
+++ devopsAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace devopsAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezzzz", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
